d64b760d79e86e1082735aef1014bac547535faf
Nathan Ross <nross.se@gmail.com>
2015-06-18 18:07:48 -0400

    able to call autoclean from any dir

--- autoclean.sh
+++ autoclean.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # requires inotify-tools
 [ $1 ] || ( echo "error, first arg must be file to watch" && exit 1 )
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 while [ -e "$1" ] && [ -f "$1" ];
 do
     inotifywait -e modify "$1";
-    python3 fodtclean.py "$1";
+    python3 ${DIR}/fodtclean.py "$1";
 done
