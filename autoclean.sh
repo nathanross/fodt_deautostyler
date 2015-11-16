b70bd2d4da35c74df81b61e80ef174ba595cfc7f
Nathan Ross <nross.se@gmail.com>
2015-11-15 21:33:05 -0500

    wrap env, pip in bash script.

--- autoclean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# requires inotify-tools
-[ $1 ] || ( echo "error, first arg must be file to watch" && exit 1 )
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-while [ -e "$1" ] && [ -f "$1" ];
-do
-    inotifywait -e modify "$1";
-    python3 ${DIR}/fodtclean.py "$1";
-done
---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+isFile() {
+    [ $1 ] && [ -e "$1" ] && [ -f "$1" ]
+    return $?
+}
+
+error() {
+    echo "$1"
+    exit 1
+}
+
+main() {
+    isFile "$1" || error "error, first arg must be file to clean"
+    [ $2 ] || error "error, second arg must be wait or once" 
+    pyvenv-3.4 env
+    source ${DIR}/env/bin/activate
+    pip install -r ${DIR}/requirements.txt
+    if [[ $2 == "wait" ]]; then
+        while [ -e "$1" ] && [ -f "$1" ];
+        do
+            inotifywait -e modify "$1";
+            python3 ${DIR}/fodtclean.py "$1";
+        done
+    else
+        python3 ${DIR}/fodtclean.py "$1";
+    fi
+}
+main $@
